5c095a70a560b8b17bb0269bf1fe6f83dbd26fe1
layslla <laysllamatias@hotmail.com>
2023-01-07 05:17:25 -0300

    atributos privados

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-class cliente {
-    nome; 
+class Cliente{
+    nome;
     cpf;
-    agencia;
-    saldo;
 }
 
-const cliente1 = new cliente(); //chamando a classe//
-const cliente2 = new cliente();
+class ContaCorrente{
+    agencia;
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
 
-// o cliente 1 e 2 são os obejtos enquanto a classe (class) é o molde//
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
 
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
+    }
+}
+
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2)
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
