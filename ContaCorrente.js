daf72c20b9249541ce90135c84769bd5c6a0fd1f
layslla <laysllamatias@hotmail.com>
2023-01-08 03:38:46 -0300

    projeto final do curso

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,12 +1,34 @@
+import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
-    cliente;
-
-
-     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
+    _cliente;
+   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+   _saldo = 0;
     
+
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
 
     sacar(valor){
         if(this._saldo >= valor){
