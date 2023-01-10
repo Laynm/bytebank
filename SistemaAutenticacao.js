9d60dc1196378e6f5742f41a4c57023c28ef7763
layslla <laysllamatias@hotmail.com>
2023-01-10 01:24:11 -0300

    aprimorando o projeto

---
+++ SistemaAutenticacao.js
@@ -0,0 +1,18 @@
+/*
+Ser autenticavel significa ter o método autenticar
+ducky type
+*/
+
+export class SistemaAutenticacao{
+    static login(autenticavel, senha){
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+       return "autenticar" in autenticavel &&
+        autenticavel.autenticar instanceof Function
+    }
+}
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+import {Cliente} from "./Cliente.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente =  new Gerente("Ricardo",  5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-const conta2 = new ContaCorrente(102, cliente2);
-
-let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
-
-console.log(contaCorrenteRicardo);
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
